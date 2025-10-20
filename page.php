f7358f61af5b4ebeadbf5894242eb8a575d463d5
jefersonluizveiga <jeferson.veiga.junior@escola.pr.gov.br>
2025-10-20 11:38:10 -0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,25 @@
+<?php
+ include "conexao.php"; // inclui o arquivo de conexao
+  ?>
+  
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./style.css">
+<title>Document</title>
+ </head>
+ <body>
+<form> 
+ <h1>login</h1> 
+ <input type="text" placeholder="Nome">
+ <input type="email" placeholder="Email"> 
+ <input type="password" placeholder="Senha"> 
+                 
+ <button type="submit"> 
+  Enviar
+ </button>
+ </form> 
+</body>
+ </html>
